8d7f032ecece35791c494457f944f1b0579eb2bc
exsyss <exsysb@gmail.com>
2024-03-17 20:53:20 +0100

    added about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,6 +1,9 @@
-export default function HomeScreen() {
+import Header from "@/components/layout/header"
+
+export default function AboutPage() {
     return (
         <div className="h-full">
+            <Header />
             <div className={`w-full h-full mii-background`}></div>
         </div>
     )
