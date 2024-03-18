1fd2c3a3f0ba68a2af7cd4555fbd11d3f75ff986
exsyss <exsysb@gmail.com>
2024-03-18 18:43:02 +0100

    added info section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,6 @@
 import HomeScreen from "@/components/home/home-screen";
+import InfoSection from "@/components/home/info-section";
+import Footer from "@/components/layout/footer";
 import Header from "@/components/layout/header";
 
 export default function Home() {
@@ -6,6 +8,8 @@ export default function Home() {
     <div className="h-full">
       <Header />
       <HomeScreen />
+      <InfoSection />
+      <Footer />
     </div>
   );
 }
