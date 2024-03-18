e1a2cd374ef361c4e3a7f0626e7f90ac612583b6
exsyss <exsysb@gmail.com>
2024-03-18 13:25:04 +0100

    added launch button on mobile

--- src/components/home/home-screen.tsx
+++ src/components/home/home-screen.tsx
@@ -1,7 +1,15 @@
+import Link from "next/link";
+
 export default function HomeScreen() {
     return (
         <div className="h-full">
-            <div className={`w-full h-full mii-background`}></div>
+            <div className={`w-full h-full mii-background`}>
+                <div className="sm:hidden h-full w-full flex justify-center items-center">
+                    <Link href={"/creator"} className="item-button !w-fit !h-fit py-2 px-7 hover:scale-95">
+                        Launch Creator
+                    </Link>
+                </div>
+            </div>
         </div>
     )
 }
