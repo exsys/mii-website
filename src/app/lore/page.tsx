8ce4ec8ddab9fa1be278dbc0ad2eb078b21844cc
exsyss <exsysb@gmail.com>
2024-03-21 18:08:35 +0100

    added footer to lore page

--- src/app/lore/page.tsx
+++ src/app/lore/page.tsx
@@ -1,3 +1,4 @@
+import Footer from "@/components/layout/footer"
 import Header from "@/components/layout/header"
 
 export default function LorePage() {
@@ -6,6 +7,9 @@ export default function LorePage() {
             <Header />
             <div className="h-full flex justify-center items-center">
                 <h1 className="text-5xl font-semibold">Coming soon...</h1>
+            </div>
+            <div className="-mt-28">
+                <Footer />
             </div>
         </div>
     )
