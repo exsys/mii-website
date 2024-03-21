827a69d4df01ecfb04ee51af587dc378b72abba8
exsyss <exsysb@gmail.com>
2024-03-21 20:00:35 +0100

    updated blob base url

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,2 +1,2 @@
-export const BASE_URL = "https://z62mtnztmn27xhnv.public.blob.vercel-storage.com";
-// example: https://z62mtnztmn27xhnv.public.blob.vercel-storage.com/items/accessory/accessory1.svg
+export const BASE_URL = "https://16cbtpc8kr2purqw.public.blob.vercel-storage.com";
+// example: https://16cbtpc8kr2purqw.public.blob.vercel-storage.com/items/accessory/accessory1.svg
