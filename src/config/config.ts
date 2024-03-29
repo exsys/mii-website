c82dbda9b63b3c085426091ca96e19f89ebb5624
exsyss <exsysb@gmail.com>
2024-03-29 17:44:10 +0100

    misc

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,3 +1,3 @@
 export const BASE_URL = "https://orange-pale-condor-574.mypinata.cloud/ipfs/QmPJQum4aHiAaeyQavyn84mpc1PhR47hYiP6Vg54TrEKcC";
-// example: https://16cbtpc8kr2purqw.public.blob.vercel-storage.com/items/accessory/accessory1.svg
 // old storage: https://z62mtnztmn27xhnv.public.blob.vercel-storage.com
+// example: https://z62mtnztmn27xhnv.public.blob.vercel-storage.com/items/accessory/accessory1.svg
