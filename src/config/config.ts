ba1bf4a2bbf0a31a0119e7f821db107352cae2a6
exsyss <exsysb@gmail.com>
2024-03-29 17:37:39 +0100

    changed item hosting to ipfs

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,2 +1,3 @@
-export const BASE_URL = "https://z62mtnztmn27xhnv.public.blob.vercel-storage.com";
+export const BASE_URL = "https://orange-pale-condor-574.mypinata.cloud/ipfs/QmfLasWgggsvQRP89k4XBCn2YNbvkFzwdkRtNpEBw37DUL";
 // example: https://16cbtpc8kr2purqw.public.blob.vercel-storage.com/items/accessory/accessory1.svg
+// old storage: https://z62mtnztmn27xhnv.public.blob.vercel-storage.com
