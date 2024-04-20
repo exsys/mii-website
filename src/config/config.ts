3eeeafc66d8f3f8b24c32da87862696e4ea55acb
exsyss <exsysb@gmail.com>
2024-04-20 15:08:55 +0200

    updated img storage

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,3 +1,3 @@
-export const BASE_URL = "https://orange-pale-condor-574.mypinata.cloud/ipfs/QmPJQum4aHiAaeyQavyn84mpc1PhR47hYiP6Vg54TrEKcC";
+export const BASE_URL = "https://z62mtnztmn27xhnv.public.blob.vercel-storage.com";
 // old storage: https://z62mtnztmn27xhnv.public.blob.vercel-storage.com
 // example: https://z62mtnztmn27xhnv.public.blob.vercel-storage.com/items/accessory/accessory1.svg
